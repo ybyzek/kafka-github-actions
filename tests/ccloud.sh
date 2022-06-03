32a8a8921eda393cda8586286ef4a1ae5390119e
Yeva Byzek <yeva@confluent.io>
2022-06-03 08:46:03 -0400

    test

--- tests/ccloud.sh
+++ tests/ccloud.sh
@@ -2,6 +2,8 @@
 
 TOPIC=$1
 
+echo "CONFLUENT_BOOTSTRAP_SERVERS: $CONFLUENT_BOOTSTRAP_SERVERS"
+
 docker run -t -e TOPIC=$TOPIC -e BOOTSTRAP=${CONFLUENT_BOOTSTRAP_SERVERS} -e APIKEY=${CONFLUENT_API_KEY} -e APISECRET=${CONFLUENT_API_SECRET} --name confluent-cli confluentinc/confluent-cli bash -c \
-  'confluent context create context-test --bootstrap ${BOOTSTRAP} --api-key ${APIKEY} --api-secret ${APISECRET} && confluent kafka topic consume -b --context context-test $TOPIC' \
+  'echo "bootstrap: $BOOTSTRAP" && confluent context create context-test --bootstrap ${BOOTSTRAP} --api-key ${APIKEY} --api-secret ${APISECRET} && confluent kafka topic consume -b $TOPIC' \
   || code=$?; if [[ $code -ne 124 && $code -ne 0 ]]; then exit $code; fi
