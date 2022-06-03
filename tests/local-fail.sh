c0d71a2a693020693b3db1922fe248d0a32a1b34
Yeva Byzek <yeva@confluent.io>
2022-06-03 06:57:45 -0400

    Local test

---
+++ tests/local-fail.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+[[ $(docker exec broker bash -c 'kafka-console-consumer --bootstrap-server localhost:9092 --topic test2 --from-beginning --max-messages 10 --timeout-ms 10000' 2>&1 | grep "Processed a total of 10 messages") == "Processed a total of 10 messages" ]]
