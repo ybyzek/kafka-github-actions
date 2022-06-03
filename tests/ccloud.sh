d84995c7b17bc9f5107ceff987acedad558668c9
Yeva Byzek <yeva@confluent.io>
2022-06-03 09:09:14 -0400

    Test

--- tests/ccloud.sh
+++ tests/ccloud.sh
@@ -4,6 +4,5 @@ TOPIC=$1
 
 echo "CONFLUENT_BOOTSTRAP_SERVERS: $CONFLUENT_BOOTSTRAP_SERVERS"
 
-docker run -t -e TOPIC=$TOPIC -e BOOTSTRAP=${CONFLUENT_BOOTSTRAP_SERVERS} -e APIKEY=${CONFLUENT_API_KEY} -e APISECRET=${CONFLUENT_API_SECRET} --name confluent-cli confluentinc/confluent-cli bash -c \
-  'echo "bootstrap: $BOOTSTRAP" && confluent context create context-test --bootstrap ${BOOTSTRAP} --api-key ${APIKEY} --api-secret ${APISECRET} && confluent kafka topic consume -b $TOPIC' \
-  || code=$?; if [[ $code -ne 124 && $code -ne 0 ]]; then exit $code; fi
+# Test for 10th message in topic
+[[ $(docker run -t -e TOPIC=$TOPIC -e BOOTSTRAP=${CONFLUENT_BOOTSTRAP_SERVERS} -e APIKEY=${CONFLUENT_API_KEY} -e APISECRET=${CONFLUENT_API_SECRET} --name confluent-cli confluentinc/confluent-cli bash -c 'echo "bootstrap: $BOOTSTRAP" && confluent context create context-test --bootstrap ${BOOTSTRAP} --api-key ${APIKEY} --api-secret ${APISECRET} && confluent context use context-test && timeout 10s confluent kafka topic consume -b $TOPIC') =~ '{"count": 9}' ]]
