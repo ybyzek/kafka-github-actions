47daa1773a94842cbca4f0023be0ad6bd4ed95f9
Yeva Byzek <yeva@confluent.io>
2022-06-03 14:24:14 -0400

    Try SR

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ See [GitHub workflow](.github/workflows/python-kafka-app.yml)
 
 To Do
 
-- [ ] cp-all-in-one wget
 - [ ] Other actions, e.g. https://github.com/marketplace/actions/schema-validation
+- [ ] Schema Registry & AVRO
 - [ ] Update README
--- producer_ccsr.py
+++ producer_ccsr.py
@@ -19,13 +19,16 @@
 #
 # Produce messages to Confluent Cloud
 # Using Confluent Python Client for Apache Kafka
+# Writes Avro data, integration with Confluent Cloud Schema Registry
 #
 # =============================================================================
+from confluent_kafka import SerializingProducer
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
 
-from confluent_kafka import Producer, KafkaError
 import json
 import ccloud_lib
-
 
 if __name__ == '__main__':
 
@@ -35,13 +38,30 @@ if __name__ == '__main__':
     topic = args.topic
     conf = ccloud_lib.read_ccloud_config(config_file)
 
-    # Create Producer instance
-    producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
-    producer = Producer(producer_conf)
+    # Create topic if needed
+    ccloud_lib.create_topic(conf, topic)
 
-    # Manually create topic if not running unit tests with mock cluster
-    if 'test.mock.num.brokers' not in conf.keys():
-      ccloud_lib.create_topic(conf, topic)
+    # for full list of configurations, see:
+    #  https://docs.confluent.io/platform/current/clients/confluent-kafka-python/#schemaregistryclient
+    schema_registry_conf = {
+        'url': conf['schema.registry.url'],
+        'basic.auth.user.info': conf['basic.auth.user.info']}
+
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+
+    name_avro_serializer = AvroSerializer(schema_registry_client = schema_registry_client,
+                                          schema_str = ccloud_lib.name_schema,
+                                          to_dict = ccloud_lib.Name.name_to_dict)
+    count_avro_serializer = AvroSerializer(schema_registry_client = schema_registry_client,
+                                           schema_str =  ccloud_lib.count_schema,
+                                           to_dict = ccloud_lib.Count.count_to_dict)
+
+    # for full list of configurations, see:
+    #  https://docs.confluent.io/platform/current/clients/confluent-kafka-python/#serializingproducer
+    producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    producer_conf['key.serializer'] = name_avro_serializer
+    producer_conf['value.serializer'] = count_avro_serializer
+    producer = SerializingProducer(producer_conf)
 
     delivered_records = 0
 
@@ -61,14 +81,15 @@ if __name__ == '__main__':
                   .format(msg.topic(), msg.partition(), msg.offset()))
 
     for n in range(10):
-        record_key = "alice"
-        record_value = json.dumps({'count': n})
-        print("Producing record: {}\t{}".format(record_key, record_value))
-        producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
-        # p.poll() serves delivery reports (on_delivery)
-        # from previous produce() calls.
+        name_object = ccloud_lib.Name()
+        name_object.name = "alice"
+        count_object = ccloud_lib.Count()
+        count_object.count = n
+        print("Producing Avro record: {}\t{}".format(name_object.name, count_object.count))
+        producer.produce(topic=topic, key=name_object, value=count_object, on_delivery=acked)
         producer.poll(0)
 
     producer.flush()
 
     print("{} messages were produced to topic {}!".format(delivered_records, topic))
+
