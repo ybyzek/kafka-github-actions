157c8b179e9d1b4c62aed4153ec032700eab4669
Yeva Byzek <yeva@confluent.io>
2022-06-03 07:25:29 -0400

    Pass in env to docker

--- tests/local.sh
+++ tests/local.sh
@@ -3,4 +3,4 @@
 TOPIC=$1
 
 # Test for 10 messages in topic
-[[ $(docker exec broker bash -c 'kafka-console-consumer --bootstrap-server localhost:9092 --topic $TOPIC --from-beginning --max-messages 10 --timeout-ms 10000' 2>&1 | grep "Processed a total of 10 messages") == "Processed a total of 10 messages" ]]
+[[ $(docker exec -e TOPIC=$TOPIC broker bash -c 'kafka-console-consumer --bootstrap-server localhost:9092 --topic $TOPIC --from-beginning --max-messages 10 --timeout-ms 10000' 2>&1 | grep "Processed a total of 10 messages") == "Processed a total of 10 messages" ]]
