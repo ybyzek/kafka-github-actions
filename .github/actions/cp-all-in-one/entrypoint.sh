9e6826fdd09f388d67b58ae6ec98b8f570147d50
Yeva Byzek <yeva@confluent.io>
2022-06-05 14:42:42 -0400

    rename project name

--- .github/actions/cp-all-in-one/entrypoint.sh
+++ .github/actions/cp-all-in-one/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "input_project: $INPUT_PROJECT"
+
 wget -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/$INPUT_GITHUB_BRANCH_VERSION/cp-all-in-one/docker-compose.yml
 docker-compose -p $INPUT_PROJECT up -d $INPUT_SERVICE
 sleep 20
