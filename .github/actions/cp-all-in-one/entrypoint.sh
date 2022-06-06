3b224a3373354732f93c6c5b517ea64dc940db05
Yeva Byzek <yeva@confluent.io>
2022-06-05 22:12:08 -0400

    Test default network name

--- .github/actions/cp-all-in-one/entrypoint.sh
+++ .github/actions/cp-all-in-one/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-echo "input_project: $INPUT_PROJECT"
-
 wget -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/$INPUT_GITHUB_BRANCH_VERSION/cp-all-in-one/docker-compose.yml
-docker-compose -p $INPUT_PROJECT up -d $INPUT_SERVICE
+#docker-compose -p $INPUT_PROJECT up -d $INPUT_SERVICE
+docker-compose up -d $INPUT_SERVICE
 sleep 20
